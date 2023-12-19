3b09b8c4c0ccda00e0b07f980ee760b1dd6e4bd9
adityajadhavv18 <aadityaj.sas@gmail.com>
2023-12-19 21:04:48 +0530

    first occurence of code done

---
+++ Arrays/BInary Search/Questions on Binary Search/Question1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int firstoccurence(int arr[], int n, int key)
+{
+    int s = 0, e = n - 1;
+    int mid = s - (e - s) / 2;
+    int ans = -1;
+    while (s <= e)
+    {
+        if (arr[mid] == key)
+        {
+            ans = mid;
+            e = mid - 1;
+        }
+        else if (key > arr[mid])
+        {
+            s = mid + 1;
+        }
+        else
+        {
+            s = mid - 1;
+        }
+        mid = s + (e - s) / 2;
+    }
+    return ans;
+}
+
+int main()
+{
+    int even[5] = {1, 2, 3, 4, 5};
+    cout << "first occ " << firstoccurence(even, 5, 3) << endl;
+    return 0;
+}
