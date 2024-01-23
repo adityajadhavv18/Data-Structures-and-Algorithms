e478b89b5cf3bbfdac12daad13d8e3b3feb4d6a1
adityajadhavv18 <aadityaj.sas@gmail.com>
2024-01-23 10:58:38 +0530

    #bubble sort done

---
+++ Arrays/Sorting/bubblesort.cpp
@@ -0,0 +1,29 @@
+/*
+It is very simple sorting technique where rounds are n-1 and in every round we compare element on the  basis of two conditions
+if a<b ignore and if a>b then swap
+in every i^th round of bubble sort its i^th largest elemnt is placed at the right place
+i=o till i=n-i
+time complexity O(n^2) best case = O(n) worst case O(n^2)
+space complexity constant
+*/
+#include <iostream>
+using namespace std;
+void bubblesort(int arr, int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        bool swapped = false;
+        for (int j = 0; j < n - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+        if (swapped == false)
+        {
+            break;
+        }
+    }
+}
